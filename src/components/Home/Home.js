3412a15346c45f09d80b747969c5805383f1184e
faisalahmed7 <faisalahmeddiu@gmail.comm>
2022-04-04 18:18:48 +0600

    Reviews in Home Page Added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,15 @@
 import React from 'react';
-
-
+import { Link } from 'react-router-dom';
+import useReviews from '../../customHooks/useReviews';
 import laptop from '../../image/laptop.jpg'
+import ReviewDetails from '../ReviewDetails/ReviewDetails'
 
 const Home = () => {
-    
+
+    const [reviews] = useReviews()
+    const newReviews=[...reviews]
+    const review=newReviews.slice(0,3)
+
     return (
         <div>
             <div className='flex justify-between mt-16 mb-20'>
@@ -29,9 +34,21 @@ const Home = () => {
                     <img className="w-4/6 2 ml-20 rounded-xl border-2" src={laptop} alt=""></img>
                 </div>
             </div>
-            
 
 
+            <div>
+                <h2 className='text-center text-5xl text-black p-3 mt-10 font-serif'>Reviews</h2>
+
+                <div className='mt-10 md:ml-60 p-3 mx-5 grid md:grid-cols-3 bg-white gap-6'>
+                {
+                     review.map(review=> <ReviewDetails key={review.id} review={review} ></ReviewDetails> )
+                 }
+                </div>
+
+                <div className='mt-10 mb-20 text-center'>
+            <Link to='/reviews' className='text-xl mt-5 border-0 rounded-lg bg-blue-400 p-3 mx-auto font-serif'>See All Reviews</Link>
+            </div>
+            </div>
 
 
         </div>
