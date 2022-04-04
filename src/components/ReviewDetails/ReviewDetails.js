bdd9806bb8e870d1ea8f4c0b8bf4cc28ec9d0a4b
faisalahmed7 <faisalahmeddiu@gmail.comm>
2022-04-04 17:50:46 +0600

    Reviews in Review Routen Added

--- src/components/ReviewDetails/ReviewDetails.js
+++ src/components/ReviewDetails/ReviewDetails.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
-const ReviewDetails = () => {
+const ReviewDetails = ({review}) => {
+    
+    const{name,description,img,ratings}=review
     return (
-        <div>
+        <div className='bg-green-100 rounded-lg p-4 w-72 h-72 text-justify'>
+            <div className='flex justify-center'>
+            <img className='rounded-full w-10 h-10' src={img} alt=''></img>
+            </div>
+            <div>
+            <h5 className='text-xl font-mono text-center mt-5 mb-3'> {name}</h5>
+            <p className='text-left font-serif'>{description}</p>
+            <p>Ratings: {ratings}</p>
+            </div>
             
         </div>
     );
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import useReviews from '../../customHooks/useReviews';
+import ReviewDetails from '../ReviewDetails/ReviewDetails';
+
+
 
 const Reviews = () => {
+const[reviews]=useReviews();
+console.log(reviews)
+
+    
     return (
         <div>
-            <h1>this is review</h1>
+            <h2 className='text-4xl text-cyan-700 text-center'>Total Review: {reviews.length}</h2>
+
+            <div className='mt-10  md:ml-60 p-3 mx-5 grid md:grid-cols-3 bg-white gap-6'>
+                {
+                    reviews.map(review=><ReviewDetails key={review.id} review={review}></ReviewDetails>)
+                }
+            </div>
+            
         </div>
     );
 };
